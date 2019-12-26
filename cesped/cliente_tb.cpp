7be3ebdd9d4e164ae35b64888f240524b550b0c0
José L. Domenech <jldm.upm@gmail.com>
2019-12-26 19:15:12 +0100

    Cliente para acceder a un servicio IoT ThingsBoard.
    * cesped/cliente_tb.h, cesped/cliente_tb.cpp

---
+++ cesped/cliente_tb.cpp
@@ -0,0 +1,64 @@
+/* -*- mode: c++; coding: utf-8 -*- */
+//
+////////////////////////////////////////////////////////////////////////////
+// Fichero:    cliente_tb.cpp
+// -------------------------------------------------------------------------
+// Proyecto:     C.E.S.P.E.D.
+// Autor:      José L. Domenech
+// Descripción:
+//   Mantiene los datos de conexión con un servicio thingsboard
+//
+// Referencia a HTTPClient para ESP32:
+// https://github.com/espressif/arduino-esp32/blob/master/libraries/HTTPClient/src/HTTPClient.h
+// -------------------------------------------------------------------------
+// Historia: + 18/12/2019 - Primera versión
+////////////////////////////////////////////////////////////////////////////
+
+#include "cliente_tb.h"
+
+#include <regex>
+
+// path de la URI de la api de telemetria
+String TB_API_TELEMETRIA = "/api/v1/_TOKEN_DISPOSITIVO_/telemetry/";
+// path de la URI de la api de alarmas 
+String TB_API_ALARMAS = "/api/alarm/_DEVICE_TYPE_/_DEVICE_ID_/";
+
+// Cambia los parámetros de conexión:
+//  - host: host del servicio
+//  - puerto: puerto del servicio
+//  - auth: token de autenticación
+//  - proto: protocolo usado: Protocolo_t::http ó Protocolo_t::https
+bool ClienteTBClass::begin(String host, int puerto, String auth, Protocolo_t proto)
+{
+     this->proto = proto;
+     this->host = host;
+     this->puerto = puerto;
+     this->auth = auth;
+};
+
+// envia al dispositivo indentificado por token el dato JSON.
+//  - token_dispositivo: identificador del dispositivo en el servicio
+//  - json: json a enviar. p.e.: "{'nombre': 'valor'}"
+//
+// devuelve el código HTTP devuelto por el servidor
+int ClienteTBClass::enviar_telemetria(String token_dispositivo, String json)
+{
+     String uri_path = String(TB_API_TELEMETRIA);
+     uri_path.replace("_TOKEN_DISPOSITIVO_", token_dispositivo);
+     
+     // iniciar comunicación con el servidor
+     this->http.begin(this->host, this->puerto, uri_path);
+     
+     // indicar las cabeceras necesarias de la API:
+     this->http.addHeader("Content-Type","application/json");
+     this->http.addHeader("X-Authorization", "Bearer " + this->auth);
+     
+     // enviar la petición y devolver el código
+     return this->http.POST(json);
+};
+
+
+// objeto interfaz
+ClienteTBClass ClienteTB;
+
+
---
+++ cesped/cliente_tb.h
@@ -0,0 +1,62 @@
+/* -*- mode: c++; coding: utf-8 -*- */
+//
+////////////////////////////////////////////////////////////////////////////
+// Fichero:    cliente_tb.h
+// -------------------------------------------------------------------------
+// Proyecto:     C.E.S.P.E.D.
+// Autor:      José L. Domenech
+// Descripción:
+//
+//   Mantenimiento de los datos de conexión con un servicio thingsboard y acceso
+//   a la API del servicio.
+//
+//   Clase ClienteTBClass: mantiene los datos de conexión con el servicio ThingsBoard
+//
+//    + begin: cambia los parámetros de conexión: host, puerto, token de
+//    autenticación que se utilizaran al hacer las peticiones de la API
+//    + enviar_telemetria: envia un dato String con formato JSON.
+//   ClienteTB: Objeto interfaz
+// -------------------------------------------------------------------------
+//   Historia: + 18/12/2019 - Primera versión
+////////////////////////////////////////////////////////////////////////////
+#ifndef CLIENTE_THINGSBOARD_HPP
+#define CLIENTE_THINGSBOARD_HPP
+
+#include "Arduino.h"
+#include "HTTPClient.h"
+
+enum class Protocolo_t { http, https };
+
+// Clase para mantener los datos de conexión a un servicio ThingsBoard
+class ClienteTBClass
+{
+public:
+     // Cambia los parámetros de conexión:
+     //  - host: host del servicio
+     //  - puerto: puerto del servicio
+     //  - auth: token de autenticación
+     //  - proto: protocolo usado: "https" o "http"
+     //
+     // devuelve true si se consiguió la conexión
+     bool begin(String host, int puerto, String auth, Protocolo_t proto=Protocolo_t::http);
+
+     // envia al dispositivo indentificado por token el dato JSON.
+     //  - token_dispositivo: identificador del dispositivo en el servicio
+     //  - json: json a enviar. p.e.: "{'nombre': 'valor'}"
+     //
+     // devuelve el codigo HTTP devuelto por el servidor
+     int enviar_telemetria(String token_dispositivo, String json);
+private:
+     // propiedades:
+     Protocolo_t proto;
+     String host;
+     int puerto;
+     String auth;
+
+     HTTPClient http; // instantacia de HTTPClient
+};
+
+// objeto interfaz
+extern ClienteTBClass ClienteTB;
+
+#endif
