aac5fb6af4cb1a215cf2358e7659ef0d9b17d43b
José L. Domenech <jldm.upm@gmail.com>
2019-12-28 13:13:47 +0100

    Ejemplo más útil de tarea.
    * cesped/cesped.ino, cesped/ejemplos/cesped.tarea.ino
    Mostrar como se puede parar una tarea.

--- cesped/cesped.ino
+++ cesped/cesped.ino
@@ -7,19 +7,30 @@
 // Autor:      José L. Domenech
 // Descripción:
 //
-//   Ejemplo de conectar a una red WiFi.
+//   Ejemplo de crear una tarea.
 // -------------------------------------------------------------------------
 //   Historia: + 18/12/2019 - Primera versión
+//             + 28/12/2019 - Versión más interesante
 ////////////////////////////////////////////////////////////////////////////
 
-#include "conexion.h"
+#include "tareas.h"
 
 void setup()
 {
         Serial.begin(9600);
         delay(1000);
         
-        Conexion.begin("", "");
+        static ptr_Tarea p_mi_tarea;
+        p_mi_tarea = new Tarea(
+                "Prueba", // nombre
+                1500,     // periodo ms
+                1,        // prioridad
+                []() -> int { return 10; }, // fun. lectora
+                [](int val) -> char* { return "Hola"; }, // fun. trans.
+                [](char* val) -> char* { Serial.println(val); return val; }); // fun. envio
+        delay(5000);
+        p_mi_tarea->parar();
+        Serial.println("Padada Tarea");
 }
 
 void loop()
--- cesped/ejemplos/cesped.tarea.ino
+++ cesped/ejemplos/cesped.tarea.ino
@@ -10,17 +10,17 @@
 //   Ejemplo de crear una tarea.
 // -------------------------------------------------------------------------
 //   Historia: + 18/12/2019 - Primera versión
+//             + 28/12/2019 - Versión más interesante
 ////////////////////////////////////////////////////////////////////////////
 
 #include "tareas.h"
-
-ptr_Tarea p_mi_tarea;
 
 void setup()
 {
         Serial.begin(9600);
         delay(1000);
         
+        static ptr_Tarea p_mi_tarea;
         p_mi_tarea = new Tarea(
                 "Prueba", // nombre
                 1500,     // periodo ms
@@ -28,7 +28,9 @@ void setup()
                 []() -> int { return 10; }, // fun. lectora
                 [](int val) -> char* { return "Hola"; }, // fun. trans.
                 [](char* val) -> char* { Serial.println(val); return val; }); // fun. envio
-        
+        delay(5000);
+        p_mi_tarea->parar();
+        Serial.println("Padada Tarea");
 }
 
 void loop()
