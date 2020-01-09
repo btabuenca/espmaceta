fd363571d0add731a3f6089980d9172c1bb29a5a
José L. Domenech <jldm.upm@gmail.com>
2020-01-09 15:27:15 +0100

    Añadir tratamiento de eventos para la conexión WiFi.
    * cesped/conexion.cpp (WiFiEstacionDesconectada, ConexionClass::conectar)
    Si se produce una dexconexión, el sistema intentará reconectar tras 3 minutos.

--- cesped/conexion.cpp
+++ cesped/conexion.cpp
@@ -13,12 +13,20 @@
 ////////////////////////////////////////////////////////////////////////////
 #include "conexion.h"
 
-// Conecta a una red WiFi con el SSID y contraseña indicada.
-void ConexionClass::begin(String ssid, String password)
+void WiFiEstacionDesconectada()
 {
-     this->ssid = ssid;
-     this->password = password;
+     // si se ha detectado una desconexión...
+     do {
+          // ... esperar X min. ...
+          delay(TIEMPO_ESPERA_RECONEXION_MS);
+          // ... e intentar la conexión ...
+          Conexion.conectar();
+     } while (WiFi.status() == WL_CONNECTED);
+     // ... hasta conseguirla.
+}
 
+int ConexionClass::conectar()
+{
      WiFi.disconnect(true); // desconectar si previamente estaba conectado.
 
      WiFi.mode(WIFI_MODE_STA); // modo estación (STA=Station), desactiva modo AP
@@ -29,10 +37,28 @@ void ConexionClass::begin(String ssid, String password)
           delay(500);
           Serial.print(".");
      }
-     Serial.println();
-     Serial.print("¡Conectado! IP: ");
-     Serial.println(WiFi.localIP());
-};
+     
+     if Serial.available()
+     {
+          Serial.println();
+          Serial.print("¡Conectado! IP: ");
+          Serial.println(WiFi.localIP());
+     }
+     
+     return WiFi.status();
+}
+
+// Conecta a una red WiFi con el SSID y contraseña indicada.
+void ConexionClass::begin(String ssid, String password)
+{
+     this->ssid = ssid;
+     this->password = password;
+
+     this->conectar();
+     // registrar una función que actue en caso de que ocurra una desconexión
+     // IDEA tomada de: https://github.com/espressif/esp-idf/blob/master/docs/en/api-guides/wifi.rst#wifi-event-sta-disconnected
+     WiFi.on(WiFiEstacionDesconectada, WIFI_EVENT_STA_DISCONNECTED);
+}
 
 // objeto interfaz
 ConexionClass Conexion;
