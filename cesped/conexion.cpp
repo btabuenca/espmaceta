e611166b3dcfdbe6fca009f8605d692198ca724c
José L. Domenech <jldm.upm@gmail.com>
2020-01-21 00:02:26 +0100

    Posibilidad de configurar el dispositivo con una IP estática
    * cesped/conexion.h, cesped/conexion.cpp (begin, conexion, IPFija):
      el método begin puede recibir un parámetro adicional: un puntero a una
      instancia de IPFija, que contendrá los parámetros de configuración necesarios
      para configurar el dispositivo con una ip estática.
    
      IPFija es una nueva clase que se utilizará para pasar los datos de
      configuración IP estática. Sus componentes, de tipo IPAddress (su
      constructor admite 4 enteros del 0 al 255), son:
        local_IP, gateway, subnet, primaryDNS, secondaryDNS

--- cesped/conexion.cpp
+++ cesped/conexion.cpp
@@ -10,7 +10,8 @@
 //
 // -------------------------------------------------------------------------
 //   Historia: + 18/12/2019 - Primera versión
-//             + 08/01/2020 - COrregir el manejo de eventos WiFi
+//             + 08/01/2020 - Corregir el manejo de eventos WiFi
+//             + 20/01/2020 - Añadir posibilidad de conf. IP estática
 ////////////////////////////////////////////////////////////////////////////
 #include "conexion.h"
 
@@ -26,12 +27,28 @@ void WiFiEstacionDesconectada(WiFiEvent_t event, WiFiEventInfo_t info)
           Conexion.conectar();
      } while (WiFi.status() == WL_CONNECTED);
      // ... hasta conseguirla.
-}
+};
 
+// Constructor de la estructura IPFija, que sirve para pasar los parámetros de
+// configuración de una ip estática a una ConexionClass
+IPFija::IPFija(IPAddress local_IP,IPAddress gateway,IPAddress subnet,IPAddress primaryDNS,IPAddress secondaryDNS)
+     :local_IP{local_IP}, gateway{gateway}, subnet{subnet}, primaryDNS{primaryDNS}, secondaryDNS{secondaryDNS}
+{};
+
+// conecta a una WiFi (con el ssid y password pasados en el constructor)
 int ConexionClass::conectar()
 {
      WiFi.disconnect(true); // desconectar si previamente estaba conectado.
 
+     if (this->conf_ip != nullptr)
+     {
+          // Configures static IP address
+          if (!WiFi.config(this->conf_ip->local_IP, this->conf_ip->gateway, this->conf_ip->subnet, this->conf_ip->primaryDNS, this->conf_ip->secondaryDNS)) {
+               Serial.println("STA Failed to configure");
+          };
+     };
+
+     
      WiFi.mode(WIFI_MODE_STA); // modo estación (STA=Station), desactiva modo AP
      // WiFi.begin requiere char* acabados en null:
      WiFi.begin(this->ssid.c_str(), this->password.c_str());
@@ -58,6 +75,21 @@ void ConexionClass::begin(String ssid, String password)
      this->password = password;
 
      this->conectar();
+     // registrar una función que actue en caso de que ocurra una desconexión
+     // IDEA tomada de: https://github.com/espressif/esp-idf/blob/master/docs/en/api-guides/wifi.rst#wifi-event-sta-disconnected
+     WiFi.onEvent(WiFiEstacionDesconectada, SYSTEM_EVENT_STA_DISCONNECTED);
+}
+
+// Conecta a una red WiFi con el SSID y contraseña indicada.
+// configuración de la interfaz para asignarle unos parámetros fijos.
+void ConexionClass::begin(String ssid, String password, IPFija *ip_fija)
+{
+     this->ssid = ssid;
+     this->password = password;
+     this->conf_ip = ip_fija;
+     
+     this->conectar();
+          
      // registrar una función que actue en caso de que ocurra una desconexión
      // IDEA tomada de: https://github.com/espressif/esp-idf/blob/master/docs/en/api-guides/wifi.rst#wifi-event-sta-disconnected
      WiFi.onEvent(WiFiEstacionDesconectada, SYSTEM_EVENT_STA_DISCONNECTED);
--- cesped/conexion.h
+++ cesped/conexion.h
@@ -20,6 +20,7 @@
 //   -------------------------------------------------------------------------
 //   Historia: + 18/12/2019 - Primera versión
 //             + 31/12/2019 - Añadir control de eventos de la conexión
+//             + 20/01/2020 - Añadir posibilidad de conf. IP estática
 ////////////////////////////////////////////////////////////////////////////
 #ifndef CONEXION_HPP
 #define CONEXION_HPP
@@ -31,6 +32,21 @@
 // esperar 3 min. antes de reconectar
 const int TIEMPO_ESPERA_RECONEXION_MS = 3 * 60 * 1000;
 
+// Clase auxiliar para almacenar la configuración de la IP fija
+class IPFija
+{
+public:
+     IPFija(IPAddress local_IP, IPAddress gateway, IPAddress subnet, IPAddress primaryDNS, IPAddress secondaryDNS);
+private:
+     IPAddress local_IP;
+     IPAddress gateway;
+     IPAddress subnet;
+     IPAddress primaryDNS;
+     IPAddress secondaryDNS;
+     
+     friend class ConexionClass; 
+}; // IPFija
+
 class ConexionClass
 {
 public:
@@ -39,10 +55,21 @@ public:
      //  - password: contraseña de la red WiFi protegida
      void begin(String ssid, String password);
 
-     // Realiza la conexión con el ssid y el passwd almacenado, y registra
-     // funciones para controlar eventos de la conexión.
+
+     // Cambia el ssid y el passwd he intenta una conexión nueva.
+     // configura la interfaz con un ip estática
+     //  - ssid: SSID de la red WiFi a conectar
+     //  - password: contraseña de la red WiFi protegida
+     //  - ip_fija: configuración de ip estática
+     void begin(String ssid, String password, IPFija *ip_fija);
+
+
+     // Realiza la conexión con el ssid y el passwd almacenado (y la
+     // configuración de ip estática si fue proporcionada anteriormente), y
+     // registra funciones para controlar eventos de la conexión.
      //   - devuevle el estado de la WiFi (debería ser ¡SIEMPRE! WL_CONNECTED)
      int conectar();
+
      
      // Devuelve el SSID almacenado.
      String getSSID();
@@ -50,7 +77,8 @@ public:
 private:
      // propiedades
      String ssid, password;
-};
+     IPFija *conf_ip = nullptr;
+}; // ConexionClass
 
 // objeto interfaz
 extern ConexionClass Conexion;
